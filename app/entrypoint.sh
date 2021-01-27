096171304d8d59ed224fe02b2edd169d14d0cd81
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-27 05:38:32 +0300

    fix passing all args to the main script

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,4 +3,4 @@ echo "Grant privileges to paths"
 chown -R mattermost:mattermost /mattermost /mattermost/config /config.json.save /mattermost/data /mattermost/logs /mattermost/plugins /mattermost/client/plugins
 
 echo "Run main script (start app)"
-exec su - mattermost /mm-entrypoint.sh
+exec su - mattermost /mm-entrypoint.sh "$@"
